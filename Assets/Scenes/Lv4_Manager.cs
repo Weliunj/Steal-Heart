5ec26c26b9629734d032e5ca246c73a5b65608d6
Quy <anhduyhh123@gmail.com>
2025-06-25 09:13:10 +0700

    ene

--- Assets/Scenes/Lv4_Manager.cs
+++ Assets/Scenes/Lv4_Manager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Lv4_Manager : MonoBehaviour
 {
@@ -8,7 +8,8 @@ public class Lv4_Manager : MonoBehaviour
     Player player;
     public AudioSource[] AudioSource;
 
-    private bool isDone =false;
+    private bool isDone = false;
+
     private void Start()
     {
         player = FindAnyObjectByType<Player>();
@@ -19,9 +20,9 @@ public class Lv4_Manager : MonoBehaviour
     {
         if (player != null)
         {
-            if(player.transform.position.x > Start_End[0].transform.position.x && !isDone)     //Bat dau
+            if (player.transform.position.x > Start_End[0].transform.position.x && !isDone) // Bắt đầu
             {
-                if(spawnEvent != null)
+                if (spawnEvent != null)
                 {
                     spawnEvent.SetActive(true);
                 }
@@ -30,20 +31,24 @@ public class Lv4_Manager : MonoBehaviour
                     AudioSource[0].Play();
                 }
             }
-        }
 
-        if(player.transform.position.x >= Start_End[1].transform.position.x  && player.transform.position.y <= Start_End[1].transform.position.y)
-        {
-            isDone = true;
-            AudioSource[0].Stop();
-            spawnEvent.SetActive(false);
-        }
+            if (player.transform.position.x >= Start_End[1].transform.position.x &&
+                player.transform.position.y <= Start_End[1].transform.position.y)
+            {
+                isDone = true;
+                AudioSource[0].Stop();
+                spawnEvent.SetActive(false);
+            }
 
-        if(player.transform.position.x < daidien.transform.position.x + 1f)
-        {
-            player.audioSources[5].Play();
-            player.Hp = 0;
+            // Chỉ kiểm tra chết khi chưa hoàn thành stage
+            if (!isDone && player.transform.position.x < daidien.transform.position.x + 1f)
+            {
+                if (player.Hp > 0)  // Đảm bảo chỉ xử lý chết 1 lần
+                {
+                    player.audioSources[5].Play();
+                    player.Hp = 0;
+                }
+            }
         }
     }
-
 }
